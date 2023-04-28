ed8a52334cf89687d98e20222d239db485001ab4
Sandeep <120578191+skmct@users.noreply.github.com>
2023-04-28 21:36:49 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,14 @@
+class student:
+    name='student'
+    age=0
+s1=student()
+s1.name="mango"
+s1.age=20
+s2=student()
+s2.name='nohit'
+s2.age=25
+s3=student()
+print(s1.name)
+print(s2.name)
+print(s3.name)
+
